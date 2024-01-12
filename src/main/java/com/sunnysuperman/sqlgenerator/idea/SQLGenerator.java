681bf53438ad00fa69e690681bc66b3b08c97664
Leo.Ji <jirenshi@meifute.com>
2024-01-12 18:29:10 +0800

    feat: 支持swagger文档注释

--- src/main/java/com/sunnysuperman/sqlgenerator/idea/SQLGenerator.java
+++ src/main/java/com/sunnysuperman/sqlgenerator/idea/SQLGenerator.java
@@ -3,138 +3,14 @@ package com.sunnysuperman.sqlgenerator.idea;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class SQLGenerator {
-    private static final Map<String, MysqlType> typeMapping = new HashMap<>();
-
-    static {
-        // 基本类型及包装类型
-        typeMapping.put("long", MysqlType.BIGINT);
-        typeMapping.put(Long.class.getName(), MysqlType.BIGINT);
-
-        typeMapping.put("int", MysqlType.INT);
-        typeMapping.put(Integer.class.getName(), MysqlType.INT);
-
-        typeMapping.put("short", MysqlType.SMALLINT);
-        typeMapping.put(Short.class.getName(), MysqlType.SMALLINT);
-
-        typeMapping.put("byte", MysqlType.TINYINT);
-        typeMapping.put(Byte.class.getName(), MysqlType.TINYINT);
-
-        typeMapping.put("boolean", MysqlType.BIT);
-        typeMapping.put(Boolean.class.getName(), MysqlType.BIT);
-
-        typeMapping.put("double", MysqlType.DOUBLE);
-        typeMapping.put(Double.class.getName(), MysqlType.DOUBLE);
-
-        typeMapping.put("float", MysqlType.FLOAT);
-        typeMapping.put(Float.class.getName(), MysqlType.FLOAT);
-
-        typeMapping.put("char", MysqlType.VARCHAR);
-        typeMapping.put(Character.class.getName(), MysqlType.VARCHAR);
-
-        // 字串&算术&日期
-        typeMapping.put(String.class.getName(), MysqlType.VARCHAR);
-        typeMapping.put(BigDecimal.class.getName(), MysqlType.DECIMAL);
-        Class<?>[] dateTypes = new Class<?>[]{Date.class, LocalDateTime.class, LocalDate.class};
-        for (Class<?> type : dateTypes) {
-            typeMapping.put(type.getName(), MysqlType.BIGINT);
-        }
-
-        // 枚举
-        typeMapping.put(Enumeration.class.getName(), MysqlType.TINYINT);
-    }
-
-    public static String generate(TableDefinition def) {
-        StringBuilder sql = new StringBuilder();
-        String tableName = def.name;
-        sql.append("CREATE TABLE `").append(tableName).append("` (\n");
-
-        TableColumn idColumn = def.columns.stream().filter(i -> i.primary).findAny().orElse(null);
-        List<TableColumn> columns = def.columns;
-        if (idColumn != null && def.columns.get(0) != idColumn) {
-            columns = new ArrayList<>(def.columns);
-            columns.remove(idColumn);
-            columns.add(0, idColumn);
-        }
-
-        char blank = ' ';
-        String indent = "  ";
-        String newLine = ",\n";
-        for (TableColumn column : columns) {
-            String[] columnDefinition = column.columnDefinition;
-            if (columnDefinition != null && columnDefinition.length > 0) {
-                for (String line : columnDefinition) {
-                    line = line.trim();
-                    line = StringUtil.replaceAll(line, ",", StringUtil.EMPTY);
-                    line = StringUtil.replaceAll(line, "\"", "'");
-                    sql.append(indent).append(line).append(newLine);
-                }
-            } else {
-                sql.append(indent);
-                String columnName = columnName(column, def);
-                MysqlType sqlType = ensureSqlTypeFromJavaType(column.javaType);
-                String comment = StringUtil.or(column.comment, "");
-                // `id` BIGINT NOT NULL AUTO_INCREMENT COMMENT 'ID',
-                // `name` VARCHAR(255) NOT NULL COMMENT '',
-                sql.append('`').append(columnName).append('`');
-                sql.append(blank).append(sqlType);
-                if (sqlType == MysqlType.VARCHAR) {
-                    sql.append('(').append(column.length).append(')');
-                }
-                int floatLength = getFloatLength(sqlType);
-                if (floatLength > 0) {
-                    sql.append('(').append(floatLength).append(",").append(column.precision).append(')');
-                }
-                sql.append(blank).append(column.nullable ? "DEFAULT" : "NOT").append(" NULL");
-                if (column.autoIncrement) {
-                    sql.append(blank).append("AUTO_INCREMENT");
-                }
-                sql.append(blank).append("COMMENT");
-                sql.append(blank).append("'").append(comment).append("'");
-                sql.append(newLine);
-            }
-        }
-
-        if (idColumn != null) {
-            sql.append(blank).append(blank).append("PRIMARY KEY (`").append(columnName(idColumn, def)).append("`)\n");
-        }
-
-        // 移除最后一个逗号
-        if (sql.toString().endsWith(",\n")) {
-            sql = new StringBuilder(sql.substring(0, sql.length() - 2) + "\n");
-        }
-
-        sql.append(") ENGINE = InnoDB");
-        if (idColumn != null && idColumn.autoIncrement) {
-            sql.append(" AUTO_INCREMENT=1");
-        }
-        sql.append(" DEFAULT CHARSET = utf8mb4 COMMENT = '").append(StringUtil.or(def.comment, tableName)).append("';");
-        return sql.toString();
-    }
-
-    private static String columnName(TableColumn column, TableDefinition def) {
-        if (StringUtil.isNotEmpty(column.name)) {
-            return column.name;
-        }
-        return def.mapCamelToUnderscore ? StringUtil.camel2underscore(column.javaName) : column.javaName;
-    }
-
-    private static MysqlType ensureSqlTypeFromJavaType(String javaType) {
-        MysqlType sqlType = typeMapping.get(javaType);
-        return sqlType != null ? sqlType : MysqlType.JSON;
-    }
-
-    private static int getFloatLength(MysqlType sqlType) {
-        if (sqlType == MysqlType.FLOAT) {
-            return 10;
-        }
-        if (sqlType == MysqlType.DOUBLE || sqlType == MysqlType.DECIMAL) {
-            return 20;
-        }
-        return -1;
-    }
 
     public enum MysqlType {
 
@@ -277,6 +153,136 @@ public class SQLGenerator {
             this.columns = columns;
         }
 
+    }
+
+    private static final Map<String, MysqlType> typeMapping = new HashMap<>();
+
+    static {
+        // 基本类型及包装类型
+        typeMapping.put("long", MysqlType.BIGINT);
+        typeMapping.put(Long.class.getName(), MysqlType.BIGINT);
+
+        typeMapping.put("int", MysqlType.INT);
+        typeMapping.put(Integer.class.getName(), MysqlType.INT);
+
+        typeMapping.put("short", MysqlType.SMALLINT);
+        typeMapping.put(Short.class.getName(), MysqlType.SMALLINT);
+
+        typeMapping.put("byte", MysqlType.TINYINT);
+        typeMapping.put(Byte.class.getName(), MysqlType.TINYINT);
+
+        typeMapping.put("boolean", MysqlType.BIT);
+        typeMapping.put(Boolean.class.getName(), MysqlType.BIT);
+
+        typeMapping.put("double", MysqlType.DOUBLE);
+        typeMapping.put(Double.class.getName(), MysqlType.DOUBLE);
+
+        typeMapping.put("float", MysqlType.FLOAT);
+        typeMapping.put(Float.class.getName(), MysqlType.FLOAT);
+
+        typeMapping.put("char", MysqlType.VARCHAR);
+        typeMapping.put(Character.class.getName(), MysqlType.VARCHAR);
+
+        // 字串&算术&日期
+        typeMapping.put(String.class.getName(), MysqlType.VARCHAR);
+        typeMapping.put(BigDecimal.class.getName(), MysqlType.DECIMAL);
+        Class<?>[] dateTypes = new Class<?>[] { Date.class, LocalDateTime.class, LocalDate.class };
+        for (Class<?> type : dateTypes) {
+            typeMapping.put(type.getName(), MysqlType.BIGINT);
+        }
+
+        // 枚举
+        typeMapping.put(Enumeration.class.getName(), MysqlType.TINYINT);
+    }
+
+    public static String generate(TableDefinition def) {
+        StringBuilder sql = new StringBuilder();
+        String tableName = def.name;
+        sql.append("CREATE TABLE `").append(tableName).append("` (\n");
+
+        TableColumn idColumn = def.columns.stream().filter(i -> i.primary).findAny().orElse(null);
+        List<TableColumn> columns = def.columns;
+        if (idColumn != null && def.columns.get(0) != idColumn) {
+            columns = new ArrayList<>(def.columns);
+            columns.remove(idColumn);
+            columns.add(0, idColumn);
+        }
+
+        char blank = ' ';
+        String indent = "  ";
+        String newLine = ",\n";
+        for (TableColumn column : columns) {
+            String[] columnDefinition = column.columnDefinition;
+            if (columnDefinition != null && columnDefinition.length > 0) {
+                for (String line : columnDefinition) {
+                    line = line.trim();
+                    line = StringUtil.replaceAll(line, ",", StringUtil.EMPTY);
+                    line = StringUtil.replaceAll(line, "\"", "'");
+                    sql.append(indent).append(line).append(newLine);
+                }
+            } else {
+                sql.append(indent);
+                String columnName = columnName(column, def);
+                MysqlType sqlType = ensureSqlTypeFromJavaType(column.javaType);
+                // `id` BIGINT NOT NULL AUTO_INCREMENT COMMENT 'ID',
+                // `name` VARCHAR(255) NOT NULL COMMENT '',
+                sql.append('`').append(columnName).append('`');
+                sql.append(blank).append(sqlType);
+                if (sqlType == MysqlType.VARCHAR) {
+                    sql.append('(').append(column.length).append(')');
+                }
+                int floatLength = getFloatLength(sqlType);
+                if (floatLength > 0) {
+                    sql.append('(').append(floatLength).append(",").append(column.precision).append(')');
+                }
+                sql.append(blank).append(column.nullable ? "DEFAULT" : "NOT").append(" NULL");
+                if (column.autoIncrement) {
+                    sql.append(blank).append("AUTO_INCREMENT");
+                }
+                sql.append(blank).append("COMMENT");
+                sql.append(blank).append("'").append(StringUtil.or(column.comment, StringUtil.EMPTY)).append("'");
+                sql.append(newLine);
+            }
+        }
+
+        if (idColumn != null) {
+            sql.append(blank).append(blank).append("PRIMARY KEY (`").append(columnName(idColumn, def)).append("`)\n");
+        }
+
+        // 移除最后一个逗号
+        if (sql.toString().endsWith(",\n")) {
+            sql = new StringBuilder(sql.substring(0, sql.length() - 2) + "\n");
+        }
+
+        sql.append(") ENGINE = InnoDB");
+        if (idColumn != null && idColumn.autoIncrement) {
+            sql.append(" AUTO_INCREMENT=1");
+        }
+        sql.append(" DEFAULT CHARSET = utf8mb4 COMMENT = '").append(StringUtil.or(def.comment, StringUtil.EMPTY))
+                .append("';");
+        return sql.toString();
+    }
+
+    private static String columnName(TableColumn column, TableDefinition def) {
+        if (StringUtil.isNotEmpty(column.name)) {
+            return column.name;
+        }
+        return def.mapCamelToUnderscore ? StringUtil.camel2underscore(column.javaName) : column.javaName;
+    }
+
+    private static MysqlType ensureSqlTypeFromJavaType(String javaType) {
+        MysqlType sqlType = typeMapping.get(javaType);
+        return sqlType != null ? sqlType : MysqlType.JSON;
+    }
+
+    private static int getFloatLength(MysqlType sqlType) {
+        if (sqlType == MysqlType.FLOAT) {
+            return 10;
+        }
+        if (sqlType == MysqlType.DOUBLE || sqlType == MysqlType.DECIMAL) {
+            return 20;
+        }
+        return -1;
     }
 
 }
--- src/main/java/com/sunnysuperman/sqlgenerator/idea/SQLGeneratorHandler.java
+++ src/main/java/com/sunnysuperman/sqlgenerator/idea/SQLGeneratorHandler.java
@@ -2,13 +2,33 @@ package com.sunnysuperman.sqlgenerator.idea;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.LangDataKeys;
-import com.intellij.psi.*;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.application.ReadAction;
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.progress.ProgressIndicator;
+import com.intellij.openapi.progress.ProgressManager;
+import com.intellij.openapi.progress.Task;
+import com.intellij.openapi.project.Project;
+import com.intellij.psi.JavaDirectoryService;
+import com.intellij.psi.PsiAnnotation;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiClassType;
+import com.intellij.psi.PsiDirectory;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiField;
+import com.intellij.psi.PsiPackage;
+import com.intellij.psi.PsiPrimitiveType;
+import com.intellij.psi.PsiType;
 import com.sunnysuperman.sqlgenerator.idea.SQLGenerator.TableColumn;
 import com.sunnysuperman.sqlgenerator.idea.SQLGenerator.TableDefinition;
 
-import javax.swing.*;
-import java.awt.*;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import java.awt.BorderLayout;
+import java.awt.Toolkit;
 import java.awt.datatransfer.StringSelection;
 import java.util.ArrayList;
 import java.util.Enumeration;
@@ -16,47 +36,121 @@ import java.util.List;
 import java.util.Objects;
 import java.util.stream.Stream;
 
+
 public class SQLGeneratorHandler extends AnAction {
+    private static final Logger LOG = Logger.getInstance(SQLGeneratorHandler.class);
 
     @Override
     public void actionPerformed(AnActionEvent e) {
-        Component component = e.getInputEvent().getComponent();
-        // 获取当前选中的类文件
-        PsiFile psiFile = e.getData(LangDataKeys.PSI_FILE);
-        if (!(psiFile instanceof PsiJavaFile)) {
-            alert(component, "请选择Java实体类");
-            return;
-        }
-        PsiJavaFile javaFile = (PsiJavaFile) psiFile;
-        PsiClass[] classes = javaFile.getClasses();
-        if (classes.length == 0) {
-            alert(component, "请选择Java实体类");
-            return;
-        }
-        // 遍历文件中的类
-        String sql;
+        // 获取当前选中的元素
+        PsiElement psiElement = e.getData(CommonDataKeys.PSI_ELEMENT);
+        // 选择类或包
+        List<String> sqlList = new ArrayList<>();
         try {
-            sql = generateSQLByJavaClass(classes[0]);
+            if (psiElement instanceof PsiClass) {
+                // 如果选中的是一个类
+                PsiClass psiClass = (PsiClass) psiElement;
+                String sql = generateSQLByJavaClass(psiClass, true);
+                if (sql != null) {
+                    sqlList.add(sql);
+                }
+                showSql(sqlList);
+            } else if (psiElement instanceof PsiDirectory) {
+                // 如果选中的是一个目录，检查它是否代表一个包
+                PsiDirectory psiDirectory = (PsiDirectory) psiElement;
+                PsiPackage psiPackage = JavaDirectoryService.getInstance().getPackage(psiDirectory);
+                if (psiPackage != null) {
+                    traversePackageInBackground(psiPackage, sqlList);
+                }
+            }
         } catch (SQLGenerationException ex) {
-            alert(component, ex.getMessage());
-            return;
+            alert(ex.getMessage());
+        } catch (Exception ex) {
+            LOG.error("SQLGeneratorHandler error", ex);
         }
-        showSqlDialog(sql);
     }
 
-    private String generateSQLByJavaClass(PsiClass type) throws SQLGenerationException {
+    private void traversePackageInBackground(PsiPackage psiPackage, List<String> sqlList) {
+        Project project = psiPackage.getProject();
+        // 创建后台任务
+        Task.Backgroundable task = new Task.Backgroundable(project, "Traversing package", true) {
+            public void run(ProgressIndicator progressIndicator) {
+                // 设置进度条的初始值和最大值
+                progressIndicator.setFraction(0.0);
+                progressIndicator.setIndeterminate(false);
+                // 在这里执行耗时操作
+                ReadAction.run(() -> {
+                    traversePackage(psiPackage, sqlList, progressIndicator);
+                    if (!progressIndicator.isCanceled()) {
+                        showSql(sqlList);
+                    }
+                });
+                // 当任务完成时更新进度条状态
+                progressIndicator.setFraction(1.0);
+            }
+        };
+        // 运行任务
+        ProgressManager.getInstance().run(task);
+    }
+
+    private void traversePackage(PsiPackage psiPackage, List<String> sqlList, ProgressIndicator progressIndicator) {
+        // 检查是否请求取消操作
+        if (progressIndicator.isCanceled()) {
+            return;
+        }
+        // 更新进度信息
+        progressIndicator.setText("遍历包生成SQL: " + psiPackage.getQualifiedName());
+        // 遍历包中的所有类
+        for (PsiClass psiClass : psiPackage.getClasses()) {
+            try {
+                String sql = generateSQLByJavaClass(psiClass, false);
+                if (sql != null) {
+                    sqlList.add(sql);
+                }
+            } catch (SQLGenerationException ex) {
+                // ignore
+            }
+        }
+        // 递归遍历子包
+        for (PsiPackage subPackage : psiPackage.getSubPackages()) {
+            traversePackage(subPackage, sqlList, progressIndicator);
+        }
+    }
+
+    private void showSql(List<String> sqlList) {
+        if (sqlList.isEmpty()) {
+            alert("请选择Java实体类或所在包");
+            return;
+        }
+        // 弹框提示
+        showSqlDialog(String.join("\n\n", sqlList));
+    }
+
+    private String generateSQLByJavaClass(PsiClass type, boolean throwsOnError) throws SQLGenerationException {
         PsiAnnotation entityAnnotation = type.getAnnotation("com.sunnysuperman.repository.annotation.Entity");
         if (entityAnnotation == null) {
-            throw new SQLGenerationException("类未标记@Entity");
+            if (throwsOnError) {
+                throw new SQLGenerationException("类未标记@Entity");
+            }
+            return null;
         }
         PsiAnnotation tableAnnotation = type.getAnnotation("com.sunnysuperman.repository.annotation.Table");
         if (tableAnnotation == null) {
-            throw new SQLGenerationException("类未标记@Table");
+            if (throwsOnError) {
+                throw new SQLGenerationException("类未标记@Table");
+            }
+            return null;
         }
         // 表定义
         TableDefinition def = new TableDefinition();
         def.setName(AnnotationUtils.getStringValue(tableAnnotation, "name"));
         def.setComment(AnnotationUtils.getStringValue(tableAnnotation, "comment"));
+        if (StringUtil.isEmpty(def.getComment())) {
+            PsiAnnotation apiModelAnnotation = type.getAnnotation("io.swagger.annotations.ApiModel");
+            if (apiModelAnnotation != null) {
+                def.setComment(AnnotationUtils.getStringValue(apiModelAnnotation, "value"));
+            }
+        }
         def.setMapCamelToUnderscore(AnnotationUtils.getBooleanValue(tableAnnotation, "mapCamelToUnderscore", true));
         def.setColumns(new ArrayList<>());
         // 遍历父类的字段
@@ -85,6 +179,12 @@ public class SQLGeneratorHandler extends AnAction {
             def.getColumns().add(column);
             column.setName(AnnotationUtils.getStringValue(columnAnnotation, "name"));
             column.setComment(AnnotationUtils.getStringValue(columnAnnotation, "comment"));
+            if (StringUtil.isEmpty(column.getComment())) {
+                PsiAnnotation apiModelPropsAnnotation = field.getAnnotation("io.swagger.annotations.ApiModelProperty");
+                if (apiModelPropsAnnotation != null) {
+                    column.setComment(AnnotationUtils.getStringValue(apiModelPropsAnnotation, "value"));
+                }
+            }
             column.setJavaName(field.getName());
             column.setJavaType(getFieldJavaType(field));
             column.setColumnDefinition(AnnotationUtils.getStringArrayValue(columnAnnotation, "columnDefinition"));
@@ -107,9 +207,12 @@ public class SQLGeneratorHandler extends AnAction {
 
     private String getFieldJavaType(PsiField field) throws SQLGenerationException {
         PsiType fieldType = field.getType();
+        if (fieldType instanceof PsiPrimitiveType) {
+            return fieldType.getPresentableText();
+        }
         PsiClass fieldClass = ((PsiClassType) fieldType).resolve();
         if (fieldClass == null) {
-            throw new SQLGenerationException("请确保类编译通过");
+            throw new SQLGenerationException("请确保类编译通过: " + fieldType.getCanonicalText());
         }
         // 枚举类统一转成Enumeration
         if (fieldClass.isEnum()) {
@@ -137,7 +240,7 @@ public class SQLGeneratorHandler extends AnAction {
         JButton copyButton = new JButton("拷贝SQL");
         copyButton.addActionListener(event -> {
             copyToClipboard(sql);
-            alert(dialog, "已拷贝");
+            alert("已拷贝");
             dialog.dispose();
         });
         // 构建并显示对话框
@@ -153,7 +256,7 @@ public class SQLGeneratorHandler extends AnAction {
         Toolkit.getDefaultToolkit().getSystemClipboard().setContents(new StringSelection(sql), null);
     }
 
-    private void alert(Component component, String msg) {
-        JOptionPane.showMessageDialog(component, msg);
+    private void alert(String msg) {
+        JOptionPane.showMessageDialog(null, msg);
     }
 }
